e2e51d7355139304028fa8a9b7b25950574cd5f1
Michael Jochimsen <mlj@troglodytics.org>
2018-10-09 17:53:30 -0700

    Add `run()` function to main module.
    The `run()` function will be hold the bulk of our application logic.
    We separate it from the `main()` function to permit us to test the
    application logic without needing to invoke the application.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,33 @@ use std::env::args;
 
 mod config;
 
+use config::Config;
+use std::process::exit;
+
 fn main() {
-    let config = config::Config::new(args());
-    println!("Hello, config: {:?}", config);
+    let config = Config::new(args());
+    match run(config) {
+        Ok(()) => (),
+        Err(reason) => {
+            eprintln!("{}", reason);
+            exit(1)
+        }
+    }
+}
+
+fn run(config: Config) -> Result<(), &'static str> {
+    println!("checksum config: {:?}", config);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+    use config::Config;
+
+    #[test]
+    fn fake_run() {
+        let config = Config::new(vec!("a", "list", "of", "files").iter());
+        assert_eq!(run(config), Ok(()));
+    }
 }
