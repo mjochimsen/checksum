f584756fbdfba62fbeb623d433d2162324ea4f19
Michael Jochimsen <mlj@troglodytics.org>
2018-10-10 22:04:54 -0700

    Add code to compute XOR on files.
    We add a bit of code to read a file and compute an XOR on its bytes.
    This lets us verify that we know how to read a file in chunks and also
    perform a computation on the data in it.
    
    We also add a couple of files of test data which we can use when
    computing checksums.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::env::args;
 use std::fs;
+use std::io;
+use std::io::Read;
+use std::path;
+use std::process::exit;
 
 mod config;
 
 use config::Config;
-use std::process::exit;
 
 fn main() {
     let config = Config::new(args());
@@ -21,7 +24,7 @@ fn main() {
 
 fn run(config: Config) -> Result<(), String> {
     for filename in config.files {
-        let path = std::path::Path::new(&filename);
+        let path = path::Path::new(&filename);
 
         let metadata = match fs::metadata(path) {
             Ok(metadata) => metadata,
@@ -34,19 +37,67 @@ fn run(config: Config) -> Result<(), String> {
         let size = metadata.len();
 
         println!("SIZE({}): {}", filename, size);
+
+        let xor = match xor(&path) {
+            Ok(xor) => xor,
+            Err(_error) => {
+                let error = format!("unable to read {}", filename);
+                return Err(error);
+            },
+        };
+
+        println!("XOR({}): {:02x}", filename, xor);
     }
 
     Ok(())
 }
 
+fn xor(path: &path::Path) -> Result<u8, io::Error> {
+    let mut input = fs::File::open(path)?;
+    let mut buffer = [0u8; 0x4000];
+    let mut xor: u8 = 0;
+
+    loop {
+        let count = input.read(&mut buffer)?;
+        if count > 0 {
+            xor = buffer.iter().fold(xor, |acc, byte| {
+                acc ^ byte
+            });
+        } else {
+            break;
+        }
+    }
+
+    Ok(xor)
+}
+
 #[cfg(test)]
 mod tests {
-    use super::run;
+    use super::*;
     use config::Config;
+    use std::path::Path;
 
     #[test]
     fn fake_run() {
-        let config = Config::new(vec!("Cargo.toml", "src/main.rs").iter());
+        let config = Config::new(vec!("test/zero.data", "test/random.data").iter());
         assert_eq!(run(config), Ok(()));
+    }
+
+    #[test]
+    fn xor_zero() {
+        let zero = Path::new("test/zero.data");
+        match xor(zero) {
+            Ok(value) => assert_eq!(value, 0),
+            Err(error) => assert!(false, "unexpected error: {:?}", error),
+        };
+    }
+
+    #[test]
+    fn xor_random() {
+        let random = Path::new("test/random.data");
+        match xor(random) {
+            Ok(value) => assert_eq!(value, 0x0f),
+            Err(error) => assert!(false, "unexpected error: {:?}", error),
+        };
     }
 }
